aff92aa956e106e502f145e2333740fd9682f8ad
James Kim <eodnjs2998@gmail.com>
2024-04-29 13:54:21 +0900

    removed unused tests.

--- UnityTests.Unity5/Assets/_Tests/_Shared Scripts/SceneGUIComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿// Author: Daniele Giardini - http://www.demigiant.com
-// Created: 2020/08/11
-
-using UnityEngine;
-
-public class SceneGUIComponent : MonoBehaviour
-{
-    public Transform[] tweenTargets;
-}
