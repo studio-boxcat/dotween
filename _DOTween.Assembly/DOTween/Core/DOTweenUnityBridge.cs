6c55d19843a55b79df3dedcc42d07c6be9a77685
James Kim <eodnjs2998@gmail.com>
2024-05-24 09:19:20 +0900

    wip

--- _DOTween.Assembly/DOTween/Core/DOTweenUnityBridge.cs
+++ _DOTween.Assembly/DOTween/Core/DOTweenUnityBridge.cs
@@ -1,20 +1,13 @@
-﻿// Author: Daniele Giardini - http://www.demigiant.com
-// Created: 2014/09/20 17:40
-//
-// License Copyright (c) Daniele Giardini.
-// This work is subject to the terms at http://dotween.demigiant.com/license.php
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Assertions;
 
 #pragma warning disable 1591
 namespace DG.Tweening.Core
 {
-    /// <summary>
-    /// Used to separate DOTween class from the MonoBehaviour instance (in order to use static constructors on DOTween).
-    /// Contains all instance-based methods
-    /// </summary>
     [AddComponentMenu("")]
+#if UNITY_EDITOR
+    [UnityEditor.InitializeOnLoad]
+#endif
     class DOTweenUnityBridge : MonoBehaviour
     {
         public static bool IsPlaying()
@@ -56,7 +49,7 @@ namespace DG.Tweening.Core
 
         static DOTweenUnityBridge()
         {
-            _editor_IsPlaying = UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode;
+            _editor_IsPlaying = false;
 
             UnityEditor.EditorApplication.playModeStateChanged += state =>
             {
